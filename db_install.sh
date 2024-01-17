d8828f8a7513049d40462b08a6aa126ccef8c29c
dav473 <hi20151215@gmail.com>
2024-01-17 14:24:55 -0500

    Update db_install.sh

--- db_install.sh
+++ db_install.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-if [ "$INIT" != 1 ]; then
-  #PostgreSQL
-  export DEBIAN_FRONTEND=noninteractive
-  # Install
-  apt install postgresql postgresql-contrib -y
+INIT_MARKER_FILE="/var/lib/docker/init_done"
 
-  #Start postgresql
-  service postgresql start
+# Check if the marker file exists
+if [ ! -e "$INIT_MARKER_FILE" ]; then
+    # Run initialization steps
+    echo "Running initialization steps..."
+
+    #PostgreSQL
+    export DEBIAN_FRONTEND=noninteractive
+    # Install
+    apt install postgresql postgresql-contrib -y
   
-  #Create kratos, hydra users
-  su -c "createdb kratos" postgres
-  su -c "createdb hydra" postgres
-  
-  #Create user and add access
-  su -c "psql -c \"CREATE USER kratos PASSWORD '$KRATOS_POSTGRES_PASSWORD';\"" postgres
-  su -c "psql -c \"CREATE USER hydra PASSWORD '$HYDRA_POSTGRES_PASSWORD';\"" postgres
-  su -c "psql -c \"GRANT CONNECT ON DATABASE kratos to kratos;\"" postgres
-  su -c "psql -c \"GRANT CONNECT ON DATABASE hydra to hydra;\"" postgres
-  #Set INIT to 1
- echo "export INIT=1" >> ~/.bashrc
- source ~/.bashrc
+    #Start postgresql
+    service postgresql start
+    
+    #Create kratos, hydra users
+    su -c "createdb kratos" postgres
+    su -c "createdb hydra" postgres
+    
+    #Create user and add access
+    su -c "psql -c \"CREATE USER kratos PASSWORD '$KRATOS_POSTGRES_PASSWORD';\"" postgres
+    su -c "psql -c \"CREATE USER hydra PASSWORD '$HYDRA_POSTGRES_PASSWORD';\"" postgres
+    su -c "psql -c \"GRANT CONNECT ON DATABASE kratos to kratos;\"" postgres
+    su -c "psql -c \"GRANT CONNECT ON DATABASE hydra to hydra;\"" postgres
+      
+    # Create the marker file to indicate that initialization is done
+    touch "$INIT_MARKER_FILE"
+
+    echo "Initialization complete."
 else
-  echo "------POSTGRESQL WAS ALREADY INSTALLED------"
+    echo "Initialization already done. Skipping."
 fi
