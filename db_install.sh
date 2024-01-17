a6d5253791d44dbb07bd46169d6f3c245b461fbe
dav473 <hi20151215@gmail.com>
2024-01-17 13:00:32 -0500

    Create db_install.sh

---
+++ db_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$INIT" = "1" ]; then
+#PostgreSQL
+  export DEBIAN_FRONTEND=noninteractive
+  # Install
+  apt install postgresql postgresql-contrib -y
+
+  #Start postgresql
+  service postgresql start
+  
+  #Create kratos, hydra users
+  su -c "createdb kratos" postgres
+  su -c "createdb hydra" postgres
+  
+  #Create user and add access
+  su -c "psql -c \"CREATE USER kratos PASSWORD '$POSTGRES_PASSWORD';\"" postgres
+  su -c "psql -c \"CREATE USER kratos PASSWORD '$HYDRA_POSTGRES_PASSWORD';\"" postgres
+  su -c "psql -c \"GRANT CONNECT ON DATABASE kratos to kratos;\"" postgres
+  su -c "psql -c \"GRANT CONNECT ON DATABASE hydra to hydra;\"" postgres
+else
+  echo "------POSTGRESQL WAS ALREADY INSTALLED------"
+fi
